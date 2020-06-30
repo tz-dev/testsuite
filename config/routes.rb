e4befccd5bce7a4117979ea7bbac71450167c354
tz <admin@noita-editor.com>
2020-06-30 13:10:50 +0200

    initial commit with auth from scratch and static pages

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+
+  get 'signup', to: 'users#new',        as: 'signup'
+  get 'login',  to: 'sessions#new',     as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  root 'statics#home'
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+end
